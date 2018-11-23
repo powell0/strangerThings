c6301635ec0978d700a48daad532dfaeebb37ca3
Ian Ferguson <icferguson@gmail.com>
2018-11-23 11:54:11 -0800

    Add context to test package Fix demo.py indentation

--- test/demo.py
+++ test/demo.py
@@ -4,6 +4,9 @@
 # Port of the Arduino NeoPixel library strandtest example.  Showcases
 # various animations on a strip of NeoPixels.
 
+# Set the context so that parent/sibling packages can be imported
+import context
+
 import signal
 import sys
 
@@ -14,8 +17,8 @@ from config import settings
 
 # Main program logic follows:
 if __name__ == '__main__':
-	# Create LedStrip object with default configuration (as defined in config/settings.py
-	led_strip = LedStrip()
+    # Create LedStrip object with default configuration (as defined in config/settings.py
+    led_strip = LedStrip()
 
     def handle_exit(sig, frame):
         led_strip.allOff()
@@ -26,20 +29,20 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, handle_exit)
 
     print('Running LED Strip Demo funtions')
-	print('Press Ctrl-C to quit.')
+    print('Press Ctrl-C to quit.')
 
-	while True:
-		# Color wipe animations.
-		led_strip.colorWipe(Color(255, 0, 0))  # Red wipe
-		led_strip.colorWipe(Color(0, 255, 0))  # Blue wipe
-		led_strip.colorWipe(Color(0, 0, 255))  # Green wipe
-		# Theater chase animations.
-		led_strip.theaterChase(Color(127, 127, 127))  # White theater chase
-		led_strip.theaterChase(Color(127,   0,   0))  # Red theater chase
-		led_strip.theaterChase(Color(  0,   0, 127))  # Blue theater chase
-		# Rainbow animations.
-		led_strip.rainbow()
-		led_strip.theaterChase(Color(127, 127, 127)) 
-		led_strip.rainbowCycle()
-		led_strip.theaterChase(Color(127, 127, 127)) 
-		led_strip.theaterChaseRainbow()
+    while True:
+        # Color wipe animations.
+        led_strip.colorWipe(Color(255, 0, 0))  # Red wipe
+        led_strip.colorWipe(Color(0, 255, 0))  # Blue wipe
+        led_strip.colorWipe(Color(0, 0, 255))  # Green wipe
+        # Theater chase animations.
+        led_strip.theaterChase(Color(127, 127, 127))  # White theater chase
+        led_strip.theaterChase(Color(127,   0,   0))  # Red theater chase
+        led_strip.theaterChase(Color(  0,   0, 127))  # Blue theater chase
+        # Rainbow animations.
+        led_strip.rainbow()
+        led_strip.theaterChase(Color(127, 127, 127)) 
+        led_strip.rainbowCycle()
+        led_strip.theaterChase(Color(127, 127, 127)) 
+        led_strip.theaterChaseRainbow()
